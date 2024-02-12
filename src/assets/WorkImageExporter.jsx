3ecf1e6fe2a7917edb73fb5915d9203f2102825c
Muhammad-Ali198 <muhammad_ali198@hotmail.com>
2024-02-12 18:53:53 +0500

    separated work projects images to its own exporter

---
+++ src/assets/WorkImageExporter.jsx
@@ -0,0 +1,51 @@
+// 360Alert
+
+import Alert3601 from "./images/360Alert/1.jpg?react";
+import Alert3602 from "./images/360Alert/2.jpg?react";
+import Alert3603 from "./images/360Alert/3.jpg?react";
+import Alert3604 from "./images/360Alert/4.jpg?react";
+import Alert3605 from "./images/360Alert/5.jpg?react";
+import Alert3606 from "./images/360Alert/6.jpg?react";
+import Alert3607 from "./images/360Alert/7.jpg?react";
+import Alert3608 from "./images/360Alert/8.jpg?react";
+
+export const Alert360_Data = [
+  { image: Alert3601 },
+  { image: Alert3602 },
+  { image: Alert3603 },
+  { image: Alert3604 },
+  { image: Alert3605 },
+  { image: Alert3606 },
+  { image: Alert3607 },
+  { image: Alert3608 },
+];
+
+// KEPLER Site
+
+import Kepler1 from "./images/NCS_Cus_Portal_Extrated_Images/1.jpg?react";
+import Kepler2 from "./images/NCS_Cus_Portal_Extrated_Images/2.jpg?react";
+import Kepler3 from "./images/NCS_Cus_Portal_Extrated_Images/3.jpg?react";
+import Kepler4 from "./images/NCS_Cus_Portal_Extrated_Images/4.jpg?react";
+import Kepler5 from "./images/NCS_Cus_Portal_Extrated_Images/5.jpg?react";
+import Kepler6 from "./images/NCS_Cus_Portal_Extrated_Images/6.jpg?react";
+import Kepler7 from "./images/NCS_Cus_Portal_Extrated_Images/7.jpg?react";
+import Kepler8 from "./images/NCS_Cus_Portal_Extrated_Images/8.jpg?react";
+import Kepler9 from "./images/NCS_Cus_Portal_Extrated_Images/9.jpg?react";
+import Kepler10 from "./images/NCS_Cus_Portal_Extrated_Images/10.jpg?react";
+import Kepler11 from "./images/NCS_Cus_Portal_Extrated_Images/11.jpg?react";
+import Kepler12 from "./images/NCS_Cus_Portal_Extrated_Images/12.jpg?react";
+
+export const Kepler_Data = [
+  { image: Kepler1 },
+  { image: Kepler2 },
+  { image: Kepler3 },
+  { image: Kepler4 },
+  { image: Kepler5 },
+  { image: Kepler6 },
+  { image: Kepler7 },
+  { image: Kepler8 },
+  { image: Kepler9 },
+  { image: Kepler10 },
+  { image: Kepler11 },
+  { image: Kepler12 },
+];
